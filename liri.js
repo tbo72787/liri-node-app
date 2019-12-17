b68b4acdfe1bd8a0c0340b5160a2352fed6948f5
Taylor Zettel <tzettel@gmail.com>
2019-12-16 19:23:45 -0500

    used npm to install moment and axios, required moment and axios, created command var for third command line argument, created cmdParam var for fourth command line argument, created commandSwitch function to detrmine liri command, created bandsInTown function to request data from bands in town api and parse and diplay results

--- liri.js
+++ liri.js
@@ -1,4 +1,40 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var axios = require("axios")
+var command = process.argv[2];
+var cmdParam = process.argv[3];
+var moment = require('moment');
+moment().format();
+// var spotify = new Spotify(keys.spotify);
+
+function commandSwitch() {
+  if(command === "concert-this") {
+    bandsInTown();
+  }
+}
+
+function bandsInTown() {
+  axios.get("https://rest.bandsintown.com/artists/" + cmdParam + "/events?app_id=codingbootcamp").then(
+    function(response) {
+      for(var i = 0; i < response.data.length; i++) {
+        console.log("----------------")
+        var event = response.data[i];
+        var date = moment(event.datetime, "YYYY-MM-DD hh:mm:ss").format("MM/DD/YYYY");
+        console.log(date);
+        console.log("Venue: " + event.venue.name);
+        if(event.venue.region !== "") {
+          console.log("Location: " + event.venue.city + ", " + event.venue.region + ", " + event.venue.country);
+          console.log("----------------");
+        }
+        else {
+          console.log("Location: " + event.venue.city + ", " + event.venue.country);
+          console.log("----------------");
+
+        }
+      }
+    }
+  )
+}
+
+commandSwitch();
